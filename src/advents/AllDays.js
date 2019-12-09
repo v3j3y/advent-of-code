e822da9f4e5836919cbbd3ced843e3b3d494eff7
v3j3y-rewardops <vejey@rewardops.com>
2019-12-09 17:17:21 -0500

    day1

---
+++ src/advents/AllDays.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Day1 from './Day1';
+
+class AllDays extends React.Component {
+    render(){
+       
+        return(
+            <div>
+               <ul>
+                <Day1 />
+               </ul> 
+            </div>
+        )
+    }
+}
+
+export default AllDays;
